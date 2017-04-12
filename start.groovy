2f64993fcdb4dfb80e0cbd8f0077a6e4f2a3649f
Pradeep Vemulakonda <pradeepvemulakonda@gmail.com>
2017-04-12 19:43:31 +1000

    Update start.groovy

--- start.groovy
+++ start.groovy
@@ -1,1 +1,8 @@
+@Grab(group="org.neo4j", module="neo4j-rest-graphdb", version="1.9.M04")
+import org.neo4j.rest.graphdb.RestGraphDatabase
+
+def db = new RestGraphDatabase("http://localhost:7474/db/data")
+def node = db.getNodeById(0)
+println node
+db.shutdown()
 println "Hello World"
